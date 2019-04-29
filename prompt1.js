bcfc071cea7d8ad64c78d46646346a06314e0e54
Amalia <amalia.fergin@gmail.com>
2019-04-29 13:47:30 -0700

    got the 0, 3, 6 in an array

--- prompt1.js
+++ prompt1.js
@@ -9,26 +9,52 @@
 //  N = 2     logs out 1 and 2 (in that order)
 
 
+// have that loop by number
 // if it is divisible by 3, take it out of the array
 // loop through and see the length.
 // Once you see the length ++ then modulo
 
 
-
-
 function divideBy3 (num) {
-  let result = [];
+  let divisibleNums = []
+  let i = 0;
 
-    for (let i = 0; i < num; i++) {
-      if (num % 3 === 0 / 2) {
-        console.log(num);
-      }
-
+  for (i; i < 9; i++) {
+    if (i % 3 === 0) {
+      let divideByNums = divisibleNums.push(i);
     }
-
-
-
-  return result
+  }
 }
 
-divideBy3(9)
+
+divideBy3()
+
+
+// var i=0
+// for (i=0;i<=10;i++)
+// {
+// if (i==3){continue}
+// document.write("The number is " + i)
+// document.write("<br />")
+// }
+
+
+// function divideBy3 (num) {
+//   let newNumber = num.toString().length
+//   console.log(newNumber);
+//
+//   for (let i = 0; i < newNumber; i++) {
+//     console.log('new num:', newNumber[i]);
+//   }
+//
+// }
+//
+// divideBy3(9)
+
+
+    // for (let i = 0; i < num; i++) {
+    //   if (num % 3 === 0 / 2) {
+    //     console.log(num);
+    //   }
+    //
+    // }
