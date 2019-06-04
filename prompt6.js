2b6613de1e1b746222ab2e2fba0416cad2ec49c9
Amalia <amalia.fergin@gmail.com>
2019-06-03 20:30:01 -0700

    added

--- prompt6.js
+++ prompt6.js
@@ -15,14 +15,17 @@
 
 // 1. find the length of M *
 // 2. find what is divisible in the length of M by N (modoulo)
+// empty arr and find each index of the numbers in the array [0, 3, 6, 9]
+// look through the m and have it divisible by separate elements/characters in emptyArr
+// look at single element in array aka do another loop or higher order function
+// .find() index of the numbers in the array
 // 3. print out the index characters in M.length into an empty array
 
 
 
 
 const characterByIndex = (n, m) => {
-  let emptyArr = []
-  let searchChar = n
+  let element = []
   let strLength = 0
 
 
@@ -30,8 +33,9 @@ const characterByIndex = (n, m) => {
   for (let i = 0; i <= m.length; i++) {
     strLength = i
     if(strLength % n === 0) {
-      console.log('strLength:', strLength);
+      element.push(strLength)
     }
+
   }
 
 
